ce77517eff0b9b489a1dce155734b337e7757a50
Jack Preston <jack.preston@skyscanner.net>
2018-08-29 18:34:22 +0100

    Basic home page, closes #14

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -3,5 +3,6 @@
 Clearance.configure do |config|
   config.routes = false
   config.rotate_csrf_on_sign_in = true
+  config.redirect_url = "/"
   config.mailer_sender = "reply@example.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "static_pages#home"
+
   resources :residents
   resources :rooms
 
