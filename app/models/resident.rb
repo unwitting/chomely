6e5e3f4089bba374e75e011f33de0f1275afd8d1
Jack Preston <jack.preston@skyscanner.net>
2018-08-30 22:22:25 +0100

    Up bio limit

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -3,7 +3,7 @@
 class Resident < ApplicationRecord
   belongs_to :room
   validates :name, length: { in: 1..150 }
-  validates :bio, length: { in: 10..1000 }
+  validates :bio, length: { in: 10..10000 }
   validate :dob_not_in_future
 
   private
